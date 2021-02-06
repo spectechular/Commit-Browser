7bec972af3fe378d71a52abc2b8caa7c9789420c
michaelramirez <mramirezt4@hotmail.com>
2021-02-05 23:05:41 -0600

    Set up basic databinding and layout

--- app/src/main/java/com/mike/commitbrowser/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mike/commitbrowser/ui/home/HomeFragment.kt
@@ -5,27 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.mike.commitbrowser.R
+import com.mike.commitbrowser.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home,
+            container, false
+        )
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        binding.homeViewModel = homeViewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 }
