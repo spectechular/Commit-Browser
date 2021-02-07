b304fce9bc000fd2c469be1a05d6f8bb4146f86d
michaelramirez <mramirezt4@hotmail.com>
2021-02-06 22:41:22 -0600

    added try and loading and error states. not functional

--- app/src/main/java/com/mike/commitbrowser/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mike/commitbrowser/ui/home/HomeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.*
 import com.mike.commitbrowser.model.CommitItem
 import com.mike.commitbrowser.model.ICommitRepository
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 import javax.inject.Inject
 
@@ -14,17 +15,33 @@ class HomeViewModel @Inject constructor(private var repository: ICommitRepositor
     val text = MutableLiveData<String>().apply {
         value = "Commits"
     }
-    private val _commits= MutableLiveData<List<CommitItem>>()
-    val commits : LiveData<List<CommitItem>> get() = _commits
+    private val _commits = MutableLiveData<List<CommitItem>>()
+    val commits: LiveData<List<CommitItem>> get() = _commits
 
+    private val _errorState = MutableLiveData<Boolean>(false)
+    val errorState: LiveData<Boolean> get() = _errorState
+
+    private val _loadingState = MutableLiveData<Boolean>(false)
+    val loadingState: LiveData<Boolean> get() = _loadingState
 
     init {
         loadCommits()
     }
+
     private fun loadCommits() {
-        viewModelScope.launch {
-            _commits.value = repository.getCommitItems()
+        _errorState.value = false
+        _loadingState.value = true
+        try {
+            viewModelScope.launch {
+                _commits.value = repository.getCommitItems()
+            }
+        } catch (e: Exception){
+            _errorState.value = true
         }
+        finally {
+            _loadingState.value = false
+        }
+
     }
 
 }
